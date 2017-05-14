83e95876804b46e047569e9a1d79725c89278247
Kam Chinta <kamchinta@gmail.com>
2017-05-14 15:04:49 -0400

    Coded the getsmallestwindow computation for task 3

--- src/edu/stanford/cs276/SmallestWindowScorer.java
+++ src/edu/stanford/cs276/SmallestWindowScorer.java
@@ -1,9 +1,8 @@
 package edu.stanford.cs276;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
 
 /**
  * A skeleton for implementing the Smallest Window scorer in Task 3.
@@ -11,10 +10,10 @@ import java.util.Map;
  * have to use Task 2. (You could also use Task 1, in which case, you'd probably like to extend CosineSimilarityScorer instead.)
  * Also, feel free to modify or add helpers inside this class.
  */
-public class SmallestWindowScorer extends BM25Scorer {
+public class SmallestWindowScorer extends CosineSimilarityScorer {
   
-  public SmallestWindowScorer(Map<String, Double> idfs, Map<Query,Map<String, Document>> queryDict) {
-    super(idfs, queryDict);
+  public SmallestWindowScorer(Map<String, Double> idfs) {
+    super(idfs);
   }
 
   /**
@@ -23,10 +22,47 @@ public class SmallestWindowScorer extends BM25Scorer {
    * @param q: query
    */  
   private int getWindow(Document d, Query q) {
-    /*
-     * @//TODO : Your code here
-     */
-    return -1;
+      int smallestWindow=Integer.MAX_VALUE;
+      HashSet<String> uniqTokens=new HashSet<String>();
+      uniqTokens.addAll(q.queryWords);
+      try {
+          //URL parsing
+          URL url=new URL(d.url);
+          HashSet<String> hset=new HashSet<String>();
+          hset.addAll(Arrays.asList(url.getHost().split(".")));
+          hset.addAll(Arrays.asList(url.getPath().split(".")[0].split("/")));
+          if(hset.containsAll(uniqTokens))
+              smallestWindow=hset.size();
+          hset.clear();
+          //title
+          hset.addAll(Arrays.asList(d.title.split(" ")));
+          if (hset.containsAll(uniqTokens) && hset.size()<smallestWindow)
+              smallestWindow=hset.size();
+          hset.clear();
+          //bodyhits (let's validate)
+          hset.addAll(d.body_hits.keySet());
+          if(hset.containsAll(uniqTokens) && hset.size()<smallestWindow)
+              smallestWindow=hset.size();
+          hset.clear();
+          //anchor text
+          for (String text:d.anchors.keySet()){
+              hset.addAll(Arrays.asList(text.split(" ")));
+              if (hset.containsAll(uniqTokens) && hset.size()<smallestWindow)
+                  smallestWindow=hset.size();
+              hset.clear();
+          }
+          //headers
+          for(String header:d.headers){
+              hset.addAll(Arrays.asList(header.split(" ")));
+              if(hset.containsAll(uniqTokens) && hset.size()<smallestWindow)
+                  smallestWindow=hset.size();
+              hset.clear();
+          }
+      } catch (MalformedURLException e) {
+          e.printStackTrace();
+      }
+
+    return smallestWindow;
   }
 
   
@@ -37,11 +73,13 @@ public class SmallestWindowScorer extends BM25Scorer {
    */  
   private double getBoostScore (Document d, Query q) {
     int smallestWindow = getWindow(d, q);
+    HashSet<String> uniqTokens=new HashSet<String>();
+    uniqTokens.addAll(q.queryWords);
     double boostScore = 0;
-    /*
-     * @//TODO : Your code here, calculate the boost score.
-     *
-     */
+    if (getWindow(d,q)==Integer.MAX_VALUE)
+      boostScore=1;
+    else
+        boostScore=(smallestWindow+uniqTokens.size())/smallestWindow;
     return boostScore;
   }
   
